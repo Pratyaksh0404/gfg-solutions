4fee6c65f3227b57189c120f13807d1051b4696b
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-08-27 09:17:38 +0530

    feat: solve Transform Array In-Place

---
+++ rearrange-an-array-with-o1-extra-space3142/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arrange(self,arr): 
+        n = len(arr)
+        for i in range(n):
+            arr[i] += (arr[arr[i]] % n) * n
+
+        for i in range(n):
+            arr[i] //= n
+        
