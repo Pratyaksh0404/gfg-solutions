7d3656441193fe67d83837bd27c0a175448b70ea
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-08-29 09:06:50 +0530

    feat: solve Friends Pairing Problem

--- friends-pairing-problem5425/Solution.py
+++ friends-pairing-problem5425/Solution.py
@@ -1,8 +1,12 @@
 class Solution:
     def countFriendsPairings(self, n: int) -> int:
+        
         if n <= 2:
             return n
+            
         a, b = 1, 2
+        
         for i in range(3, n + 1):
             a, b = b, b + (i - 1) * a
+            
         return b
