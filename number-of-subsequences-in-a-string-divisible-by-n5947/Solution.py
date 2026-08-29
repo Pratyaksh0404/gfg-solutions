e261bd78650969f31e08a7c1b13c369bcd254652
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-08-29 08:04:04 +0530

    feat: solve Count Subsequences Divisible by n

---
+++ number-of-subsequences-in-a-string-divisible-by-n5947/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countSubsequences(self, s: str, n: int) -> int:
+        mod = 10**9 + 7
+        dp = [0] * n
+        for c in s:
+            d = int(c)
+            nxt = list(dp)
+            for j in range(n):
+                if dp[j]:
+                    r = (j * 10 + d) % n
+                    nxt[r] = (nxt[r] + dp[j]) % mod
+            nxt[d % n] = (nxt[d % n] + 1) % mod
+            dp = nxt
+        return dp[0]
