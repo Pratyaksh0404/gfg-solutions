94127c0d782886f9619f6a5610eeaf15315ad882
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-08-31 21:00:54 +0530

    feat: solve Majority Element

---
+++ majority-element-1587115620/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, arr):
+        count = 0
+        a = None
+
+        for num in arr:
+            if count == 0:
+                a = num
+                count = 1
+            elif num == a:
+                count += 1
+            else:
+                count -= 1
+
+        if arr.count(a) > len(arr) // 2:
+            return a
+        else:
+            return -1
