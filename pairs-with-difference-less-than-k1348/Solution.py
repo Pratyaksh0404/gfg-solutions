b3f700d4426c9ffb23b8b39d3a27fb4bcdb45bf3
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-01 11:56:30 +0530

    feat: solve Pairs with Less Than K Diff

---
+++ pairs-with-difference-less-than-k1348/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPairs(self, arr: list[int], k: int) -> int:
+        if k <= 0:
+            return 0
+        arr.sort()
+        
+        n = len(arr)
+        ans = 0
+        j = 0
+        for i in range(n):
+            while j < n and arr[j] - arr[i] < k:
+                j += 1
+            ans += j - i - 1
+            
+        return ans
