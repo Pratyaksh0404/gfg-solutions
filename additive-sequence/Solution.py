2fc2c00d034f0fdb0a05dce5263f73ffbd110160
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-01 21:26:46 +0530

    feat: solve Check Sum String

--- additive-sequence/Solution.py
+++ additive-sequence/Solution.py
@@ -2,21 +2,22 @@ class Solution:
     def isSumString (self, s):
         n = len(s)
     
-        def is_valid(num):
+        def solve1(num):
             return len(num) == 1 or num[0] != '0'
     
-        def check(a, b, remaining):
-            if not remaining:
+        def solve(a, b, rem):
+            if not rem:
                 return True
-            sum_str = str(int(a) + int(b))
-            if remaining.startswith(sum_str):
-                return check(b, sum_str, remaining[len(sum_str):])
+            ss = str(int(a) + int(b))
+            if rem.startswith(ss):
+                return solve(b, ss, rem[len(ss):])
             return False
     
         for i in range(1, n):
             for j in range(i+1, n):
                 a, b = s[:i], s[i:j]
-                if is_valid(a) and is_valid(b):
-                    if check(a, b, s[j:]):
+                if solve1(a) and solve1(b):
+                    if solve(a, b, s[j:]):
                         return True
+                        
         return False
