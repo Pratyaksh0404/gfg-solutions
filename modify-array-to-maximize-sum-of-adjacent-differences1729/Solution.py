86cfe6d3d15e2e567f77d4d93bbb63c2ba5a9ac8
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-03 07:52:21 +0530

    feat: solve Max Adjacent Diffs Sum with 1 Replacements

---
+++ modify-array-to-maximize-sum-of-adjacent-differences1729/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxDiffSum(self, arr):
+        a, b = 0, 0
+        for i in range(1, len(arr)):
+            a, b = max(a + abs(arr[i] - arr[i-1]), b + abs(arr[i] - 1)), max(a + abs(1 - arr[i-1]), b)
+        
+        return max(a, b)
