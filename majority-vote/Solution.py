2764c11ec9c00575549fe8109ac96aec6540a90a
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-04 09:12:35 +0530

    feat: solve Majority Element - More Than n/3

--- majority-vote/Solution.py
+++ majority-vote/Solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def findMajority(self, arr):
         a = set(arr)
-        ans=[]
+        ans = []
         n = len(arr)
         for i in a:
             if arr.count(i) > n//3:
                 ans.append(i)
+                
         ans.sort()
         return ans
