112716424f770889a071e14cb3ad2f065589153b
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-04 09:15:21 +0530

    feat: solve Largest Rectangle with Column Swaps

--- find-the-largest-rectangle-of-1s-with-swapping-of-columns-allowed0243/Solution.py
+++ find-the-largest-rectangle-of-1s-with-swapping-of-columns-allowed0243/Solution.py
@@ -2,7 +2,6 @@ class Solution:
     def maxArea(self, mat: list[list[int]]) -> int:
         n = len(mat)
         m = len(mat[0])
-    
         for i in range(1, n):
             for j in range(m):
                 if mat[i][j] == 1:
@@ -13,12 +12,11 @@ class Solution:
             cc = [0] * (n + 1)
             for j in range(m):
                 cc[mat[i][j]] += 1
-    
-            col = 1
+            c = 1
             for h in range(n, 0, -1):
                 while cc[h] > 0:
-                    ans = max(ans, h * col)
-                    col += 1
+                    ans = max(ans, h * c)
+                    c += 1
                     cc[h] -= 1
     
         return ans
