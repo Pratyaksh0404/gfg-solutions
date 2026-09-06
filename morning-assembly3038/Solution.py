150527a7fd467cfa1aad28932a1aac4f4f4a02b9
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-06 08:27:42 +0530

    feat: solve Minimum Moves to Sort Permutation

--- morning-assembly3038/Solution.py
+++ morning-assembly3038/Solution.py
@@ -3,15 +3,15 @@ class Solution:
         pos = [0] * (len(arr) + 1)
         for i, x in enumerate(arr):
             pos[x] = i
-    
+
         l = 1
         curr = 1
-    
+
         for x in range(2, len(arr) + 1):
             if pos[x] > pos[x - 1]:
                 curr += 1
             else:
                 curr = 1
             l = max(l, curr)
-    
+
         return len(arr) - l
