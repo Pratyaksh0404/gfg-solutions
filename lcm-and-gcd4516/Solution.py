ab4e5fbdf9ce8cee1b364fa88ae1a9bafe63d971
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-12 13:10:34 +0530

    feat: solve LCM And GCD

--- lcm-and-gcd4516/Solution.py
+++ lcm-and-gcd4516/Solution.py
@@ -1,7 +1,9 @@
-import math
 class Solution:
     def lcmAndGcd(self, a : int, b : int) -> List[int]:
-        g = math.gcd(a, b)
-        l = (a * b) // g
-        return [l, g]
+        x, y = a, b
+        while y:
+            x, y = y, x % y
+        gcd = x
+        lcm = (a * b) // gcd if gcd != 0 else 0
+        return [lcm, gcd]
     
