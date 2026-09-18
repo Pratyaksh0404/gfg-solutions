6d359be173ab7f63aa4ead912b1de7fbeaf5d2ba
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-18 07:52:45 +0530

    feat: solve Minimum Absolute Difference In BST

---
+++ minimum-absolute-difference-in-bst-1665139652/Solution.py
@@ -0,0 +1,25 @@
+'''
+Binary Tree Node Structure
+class Node:
+    def __init__(self):
+        self.data = None
+        self.left = None
+        self.right = None
+'''
+        
+class Solution:
+    def absDiff(self, root):
+        self.min_diff = float('inf')
+        self.prev = None
+
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            if self.prev is not None:
+                self.min_diff = min(self.min_diff, node.data - self.prev)
+            self.prev = node.data
+            inorder(node.right)
+
+        inorder(root)
+        return self.min_diff
