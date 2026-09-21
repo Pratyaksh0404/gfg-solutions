97c3245a08114c4cb412f0656421c94a4d6734d5
Pratyaksh Agrawal <156936952+Pratyaksh0404@users.noreply.github.com>
2026-09-21 23:08:46 +0530

    feat: solve Merge Sort

---
+++ merge-sort/Solution.py
@@ -0,0 +1,36 @@
+class Solution:
+    def mergeSort(self, arr, l, r):
+        if l < r:
+            m = (l + r) // 2
+            self.mergeSort(arr, l, m)
+            self.mergeSort(arr, m + 1, r)
+            self.merge(arr, l, m, r)
+
+    def merge(self, arr, l, m, r):
+        n1 = m - l + 1
+        n2 = r - m
+
+        L = arr[l:m+1]
+        R = arr[m+1:r+1]
+
+        i = j = 0
+        k = l
+
+        while i < n1 and j < n2:
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < n1:
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < n2:
+            arr[k] = R[j]
+            j += 1
+            k += 1
